1891781735e60c9f3a24dd483f5c64e300d3f016
Ryan Murphy <TheDecoder007@gmail.com>
2022-12-01 14:03:30 -0500

    reinstalled packages and began coding

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <container className="aboutContainer" id = "about">
+            <h3 className="text-center sectionHeader">About Me</h3>
+        </container>
+
+    );
+};
+
+export default About;
